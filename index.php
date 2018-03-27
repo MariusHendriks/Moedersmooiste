db498636d1229de6262febc1796932431b4bcbb5
Mark Hendriks <mark.hendriks9999@gmail.com>
2018-03-27 11:02:39 +0200

    basic site setupt

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/.css">
+    </head>
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Biografie</a></li>
+            <li><a href="">Shows</a></li>
+            <li><a href="">Videos</a></li>
+            <li><a href="">Muziek</a></li>
+            <li><a href="">Fotos</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+    <body>
+        <main>
+            <section class="header"><?php include_once 'modules/header.php'?></section>
+            <section class="biografie"><?php include_once 'modules/biografie.php'?></section>
+            <section class="shows"><?php include_once 'modules/shows.php'?></section>
+            <section class="videos"><?php include_once 'modules/videos.php'?></section>
+            <section class="muziek"><?php include_once 'modules/muziek.php'?></section>
+            <section class="fotos"><?php include_once 'modules/fotos.php'?></section>
+            <section class="contact"><?php include_once 'modules/contact.php'?></section>
+        </main>
+    </body>
+</html>
---
+++ modules/biografie.php
@@ -0,0 +1,1 @@
+biografie test
