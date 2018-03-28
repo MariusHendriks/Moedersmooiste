026bfda2f0fec86e0264283a51737e989883c3c3
Mark Hendriks <mark.hendriks9999@gmail.com>
2018-03-28 12:59:42 +0200

    carousel dingen

--- modules/videos.php
+++ modules/videos.php
@@ -1,15 +1,84 @@
-<div class="owl-carousel">
-  <div> Your Content </div>
-  <div> Your Content </div>
-  <div> Your Content </div>
-  <div> Your Content </div>
-  <div> Your Content </div>
-  <div> Your Content </div>
-  <div> Your Content </div>
+<h1>Video</h1>
+<div class="owl-carousel owl-theme owl-loaded">
+    <div class="owl-stage-outer">
+        <div class="owl-stage">
+            <div class="owl-item">
+              <div class="item-video"><a class="owl-video" href="https://www.youtube.com/watch?v=JpxsRwnRwCQ"></a></div>
+                <h3>In De war</h3>
+              <h3>Live @ Grenswerk</h3>
+            </div>
+            <div class="owl-item">
+              <div class="imageCarousel">
+                2
+              </div>
+              <h3>Zwarte kleren</h3>
+              <h3>Live @ Grenswerk</h3>
+            </div>
+            <div class="owl-item">
+              <div class="imageCarousel">
+                3
+              </div>
+              <h3>In de war</h3>
+              <h3>Videoclip</h3>
+            </div>
+            <div class="owl-item">
+              <div class="imageCarousel">
+                4
+              </div>
+              <h3>Niets meer</h3>
+              <h3>Live @ Tryp</h3>
+            </div>
+            <div class="owl-item">
+              <div class="imageCarousel">
+                5
+              </div>
+              <h3>Titel</h3>
+              <h3>Vanue</h3>
+            </div>
+            <div class="owl-item">
+              <div class="imageCarousel">
+                6
+              </div>
+              <h3>Titel</h3>
+              <h3>Vanue</h3>
+            </div>
+            <div class="owl-item">
+              <div class="imageCarousel">
+                7
+              </div>
+              <h3>Titel</h3>
+              <h3>Vanue</h3>
+            </div>
+            <div class="owl-item">
+              <div class="imageCarousel">
+                8
+              </div>
+              <h3>Titel</h3>
+              <h3>Vanue</h3>
+            </div>
+            <div class="owl-item">
+              <div class="imageCarousel">
+                9
+              </div>
+              <h3>Titel</h3>
+              <h3>Vanue</h3>
+            </div>
+        </div>
+    </div>
+
+    <div class="owl-dots">
+
+    </div>
 </div>
 
 <script type="text/javascript">
+  var width = $('.imageCarousel').width();
   $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    $(".owl-carousel").owlCarousel({
+      center: true,
+      items:5,
+      loop:true,
+      margin:20
+    });
   });
 </script>
