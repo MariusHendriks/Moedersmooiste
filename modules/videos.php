1c87b7c1c393e54405322d66b44d882dce8c1c14
Mark Hendriks <mark.hendriks9999@gmail.com>
2018-03-29 09:06:49 +0200

    video section

--- modules/videos.php
+++ modules/videos.php
@@ -3,65 +3,60 @@
     <div class="owl-stage-outer">
         <div class="owl-stage">
             <div class="owl-item">
-              <div class="item-video"><a class="owl-video" href="https://www.youtube.com/watch?v=JpxsRwnRwCQ"></a></div>
-                <h3>In De war</h3>
+              <div class="item-video">
+                <iframe src="https://www.youtube.com/embed/1Dj_MRbBTKQ">
+                </iframe>
+              </div>
+              <h3>In De war</h3>
               <h3>Live @ Grenswerk</h3>
             </div>
             <div class="owl-item">
-              <div class="imageCarousel">
-                2
+              <div class="item-video">
+                <iframe src="https://www.youtube.com/embed/1Dj_MRbBTKQ">
+                </iframe>
               </div>
-              <h3>Zwarte kleren</h3>
+              <h3>In De war</h3>
               <h3>Live @ Grenswerk</h3>
             </div>
             <div class="owl-item">
-              <div class="imageCarousel">
-                3
+              <div class="item-video">
+                <iframe src="https://www.youtube.com/embed/1Dj_MRbBTKQ">
+                </iframe>
               </div>
-              <h3>In de war</h3>
-              <h3>Videoclip</h3>
+              <h3>In De war</h3>
+              <h3>Live @ Grenswerk</h3>
             </div>
             <div class="owl-item">
-              <div class="imageCarousel">
-                4
+              <div class="item-video">
+                <iframe src="https://www.youtube.com/embed/1Dj_MRbBTKQ">
+                </iframe>
               </div>
-              <h3>Niets meer</h3>
-              <h3>Live @ Tryp</h3>
+              <h3>In De war</h3>
+              <h3>Live @ Grenswerk</h3>
             </div>
             <div class="owl-item">
-              <div class="imageCarousel">
-                5
+              <div class="item-video">
+                <iframe src="https://www.youtube.com/embed/1Dj_MRbBTKQ">
+                </iframe>
               </div>
-              <h3>Titel</h3>
-              <h3>Vanue</h3>
+              <h3>In De war</h3>
+              <h3>Live @ Grenswerk</h3>
             </div>
             <div class="owl-item">
-              <div class="imageCarousel">
-                6
+              <div class="item-video">
+                <iframe src="https://www.youtube.com/embed/1Dj_MRbBTKQ">
+                </iframe>
               </div>
-              <h3>Titel</h3>
-              <h3>Vanue</h3>
+              <h3>In De war</h3>
+              <h3>Live @ Grenswerk</h3>
             </div>
             <div class="owl-item">
-              <div class="imageCarousel">
-                7
+              <div class="item-video">
+                <iframe src="https://www.youtube.com/embed/1Dj_MRbBTKQ">
+                </iframe>
               </div>
-              <h3>Titel</h3>
-              <h3>Vanue</h3>
-            </div>
-            <div class="owl-item">
-              <div class="imageCarousel">
-                8
-              </div>
-              <h3>Titel</h3>
-              <h3>Vanue</h3>
-            </div>
-            <div class="owl-item">
-              <div class="imageCarousel">
-                9
-              </div>
-              <h3>Titel</h3>
-              <h3>Vanue</h3>
+              <h3>In De war</h3>
+              <h3>Live @ Grenswerk</h3>
             </div>
         </div>
     </div>
@@ -70,15 +65,16 @@
 
     </div>
 </div>
-
 <script type="text/javascript">
   var width = $('.imageCarousel').width();
   $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
       center: true,
-      items:5,
+      items:3,
+      video:true,
       loop:true,
-      margin:20
+      margin:20,
+
     });
   });
 </script>
