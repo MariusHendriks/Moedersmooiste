e0a6b9f739dd99970dcd3914c1ea3f9f05105fe6
Max Altena <Max@MaxAltena.com>
2018-04-03 12:53:34 +0200

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -19,7 +19,5 @@
 <meta name="theme-color" content="#f7f7f7">
 <link href="https://fonts.googleapis.com/css?family=Prompt:700,900|Roboto:400,400i,700,700i" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="css/style.css">
-<link rel="stylesheet" href="css/owl.carousel.min.css">
-<link rel="stylesheet" href="css/owl.theme.default.min.css">
 <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
