8b1223cb2cf6155e769d2fa908ecd5cd9a87672a
Max Altena <Max@MaxAltena.com>
2018-04-18 18:30:06 +0200

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -25,4 +25,4 @@
 <script defer src="https://use.fontawesome.com/releases/v5.0.9/js/all.js" integrity="sha384-8iPTk2s/jMVj81dnzb/iFR2sdA7u06vHJyyLlAd4snFpCl/SnyUjRrbdJsw1pGIl" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<script src="scripts/owl.carousel.min.js"></script>
+<script src="/Moedersmooiste/scripts/owl.carousel.min.js"></script>
