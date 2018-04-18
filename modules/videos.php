a1a792887cab955be894c16908fb4a7c4472dcde
Max Altena <Max@MaxAltena.com>
2018-04-18 18:13:04 +0200

    updated videos.php to original

--- modules/videos.php
+++ modules/videos.php
@@ -1,6 +1,34 @@
 <div class="sectionHeader">
-    <h1>Videos</h1>
+    <h1>Muziek</h1>
 </div>
-<div class="sectionContent">
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque voluptatibus, corporis quis ea nam praesentium! Debitis libero officiis, nihil voluptatum labore atque harum doloremque, deleniti ipsa enim explicabo possimus nobis eligendi sequi veniam dolorem soluta veritatis vero alias recusandae rem, reiciendis iste. Aliquid, earum expedita fuga ea dolore laudantium placeat numquam reprehenderit. Distinctio amet in consequatur, quibusdam consectetur, obcaecati hic voluptas praesentium eum quaerat nam quis, neque aut voluptatibus voluptate illo expedita velit est sapiente sequi natus odit ea fugiat. Commodi, adipisci vero enim placeat est dolores tempora ipsa, consequuntur blanditiis incidunt. Iste quidem, ea eum nisi placeat perferendis perspiciatis.</p>
+<div class="owl-carousel owl-theme">
+    <div class="item-video" data-merge="1.3"><a class="owl-video" href="https://www.youtube.com/watch?v=ddZlaO_48og"></a></div>
+    <div class="item-video" data-merge="1.3"><a class="owl-video" href="https://www.youtube.com/watch?v=Nj3UF4zSHmA"></a></div>
+    <div class="item-video" data-merge="1.3"><a class="owl-video" href="https://www.youtube.com/watch?v=1Dj_MRbBTKQ"></a></div>
+    <div class="item-video" data-merge="1.3"><a class="owl-video" href="https://www.youtube.com/watch?v=nxkf7g_6Ljw"></a></div>
 </div>
+
+<script type="text/javascript">
+  $(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      center: true,
+      video:true,
+      loop:true,
+      margin:20,
+      lazyLoad:true,
+      merge:true,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1,
+          },
+          600:{
+              items:3,
+          },
+          1000:{
+              items:5,
+          }
+        }
+    });
+  });
+</script>
